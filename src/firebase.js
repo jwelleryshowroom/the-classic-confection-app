bfa2902d28fbeea0d199f366d816f04734d629ea
Ankit Kumar Sah <ankitkumarsah@Ankits-MacBook-Air.local>
2026-01-07 01:26:27 +0530

    feat: initial dev setup and local config

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // TODO: Replace with your actual Firebase project config
 const firebaseConfig = {
-    apiKey: "AIzaSyBSEBIYY3f8kr_oifW3NuATP7qLMyNcuSY",
-    authDomain: "shyam-bakery-app.firebaseapp.com",
-    projectId: "shyam-bakery-app",
-    storageBucket: "shyam-bakery-app.firebasestorage.app",
-    messagingSenderId: "220642985890",
-    appId: "1:220642985890:web:c11851f2b14c02737314fb",
-    measurementId: "G-B40XQJM45T"
+  apiKey: "AIzaSyBSEBIYY3f8kr_oifW3NuATP7qLMyNcuSY",
+  authDomain: "shyam-bakery-app.firebaseapp.com",
+  projectId: "shyam-bakery-app",
+  storageBucket: "shyam-bakery-app.firebasestorage.app",
+  messagingSenderId: "220642985890",
+  appId: "1:220642985890:web:c11851f2b14c02737314fb",
+  measurementId: "G-B40XQJM45T"
 };
 
 // Initialize Firebase
